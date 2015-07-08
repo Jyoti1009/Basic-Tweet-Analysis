41e92689cea4f10846d156859cefbf4e8541d8fc
Jyoti1009 <Jyoti1009@users.noreply.github.com>
2015-07-08 16:09:32 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -10,7 +10,7 @@ python ./src/median_unique.py ./tweet_input/tweets.txt ./tweet_output/ft2.txt
 # python ./src/median_unique.py twitter:none ./tweet_output/ft2.txt
 # python ./src/median_unique.py twitter:<userid> ./tweet_output/ft2.txt
 
-python ./src/extra_features.py ./tweet_input/tweets.txt ./tweet_output/urls.txt ./tweet_output/hashtags.txt ./tweet_output/replyats.txt ./tweet_output/sentiment.csv ./tweet_output/trends.txt
+# python ./src/extra_features.py ./tweet_input/tweets.txt ./tweet_output/urls.txt ./tweet_output/hashtags.txt ./tweet_output/replyats.txt ./tweet_output/sentiment.csv ./tweet_output/trends.txt
 # python ./src/extra_features.py twitter:home ./tweet_output/urls.txt ./tweet_output/hashtags.txt ./tweet_output/replyats.txt ./tweet_output/sentiment.csv ./tweet_output/trends.txt
 # python ./src/extra_features.py twitter:none ./tweet_output/urls.txt ./tweet_output/hashtags.txt ./tweet_output/replyats.txt ./tweet_output/sentiment.csv ./tweet_output/trends.txt
 # python ./src/extra_features.py twitter:<userid> ./tweet_output/urls.txt ./tweet_output/hashtags.txt ./tweet_output/replyats.txt ./tweet_output/sentiment.csv ./tweet_output/trends.txt
