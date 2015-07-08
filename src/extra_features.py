7dcfd33ecdafb712c4f7cf565fca76fd788b7df2
Jyoti1009 <Jyoti1009@users.noreply.github.com>
2015-07-08 16:35:35 +0530

    Update extra_features.py

--- src/extra_features.py
+++ src/extra_features.py
@@ -1,4 +1,5 @@
 import csv
+import time
 
 import text_utils as txu
 import twitter_utils as twu
@@ -24,19 +25,29 @@ if __name__ == '__main__':
 	inp, urls, tags, replyats, sentiment, trends = txu.extract_arguments(n=7)
 	tweets = twu.get_input(inp)
 	tweets_text = "\n".join(tweets)	
-
+	
 	# extract all URLs in the tweets and write in a file
+	start = time.clock()
 	txu.write_file('\n'.join(txu.get_urls(tweets_text)), urls)
+	print "Time taken in extracting URLs: ", time.clock() - start
 
 	# extract all hashtags in the tweets and write in a file
+	start = time.clock()
 	txu.write_file('\n'.join(txu.extract_hashtags(tweets_text)), tags)
+	print "Time taken in extracting hashtags: ", time.clock() - start
 
 	# extract all replyats in the tweets and write in a file
+	start = time.clock()
 	txu.write_file('\n'.join(txu.extract_replyat(tweets_text)), replyats)
+	print "Time taken in extracting replyats: ", time.clock() - start
 
-	# get sentiments in the tweets and write in a file	 
+	# get sentiments in the tweets and write in a file
+	start = time.clock()
 	write_csv(st.get_sentiments(tweets), sentiment)
+	print "Time taken in extracting sentiment: ", time.clock() - start
 
 	# extract all trending hashtags worldwide and write in a file
+	start = time.clock()
 	txu.write_file('\n'.join(twu.get_worldwide_trends()), trends)
+	print "Time taken in extracting trends: ", time.clock() - start
 	
